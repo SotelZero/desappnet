a0238981ec8484bef8e5fa4c377225f101a48d14
Sotelo <menio.zero@gmail.com>
2019-08-22 13:26:14 -0500

    Envio ejercicio 4

---
+++ 03.areatriangulo/Program.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace _03.areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float area, b, a;
+
+            Console.WriteLine("Programa que calcula el area de un triangulo");
+
+            if(args.Length<2) {
+                Console.WriteLine("Forma de uso \n");
+                Console.WriteLine("<base> <altura> \n");
+                Environment.Exit(0);
+            }
+
+            b = float.Parse(args[0]);
+            a = float.Parse(args[1]);
+            
+            area = (b*a)/2;
+            Console.WriteLine($"El triangulo de base {b} y altura {a} tieen un area {area} \n");
+        }
+    }
+}
---
+++ 04.pagatrabajador/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04.pagatrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int horas, horasextra;
+            float paga, tasa=0.3f;
+            float bruta, extra, impuesto, neta;
+
+            Console.Clear();
+            Console.WriteLine("Programa que calcula la paga de un trabajador");
+
+            Console.WriteLine("Nombre"); nombre=Console.ReadLine();
+            Console.WriteLine("Horas"); horas=int.Parse(Console.ReadLine());
+            Console.WriteLine("Paga"); paga=float.Parse(Console.ReadLine());
+
+            horasextra = (horas>40 ? horas-40 : 0);
+            extra = horasextra *(paga * 2);
+            bruta = (horas-horasextra)*paga+extra;
+            impuesto = bruta * tasa;
+            neta = bruta-impuesto;
+
+            Console.WriteLine($"{nombre} trabajó {horas} a una paga de {paga}");
+            Console.WriteLine($"Paga bruta {bruta}");
+            Console.WriteLine($"Impuesto {impuesto}");
+            Console.WriteLine($"Paga neta {neta}");
+        }
+    }
+}

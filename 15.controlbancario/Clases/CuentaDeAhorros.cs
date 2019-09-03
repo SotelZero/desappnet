84b83a0c8e490a32942c7958899ded18e2a404f2
Sotelo <menio.zero@gmail.com>
2019-09-03 13:21:15 -0500

    Envia

---
+++ 15.controlbancario/Clases/CuentaDeAhorros.cs
@@ -0,0 +1,11 @@
+namespace ControlBancario.Clases
+{
+    public class CuentaDeAhorros:CuentaBancaria{
+        private double tasa;
+        public CuentaDeAhorros(double saldo, double tasa)
+            : base(saldo) //invocamos constructor clase base
+        {
+            this.tasa=tasa;
+        }
+    }
+}
